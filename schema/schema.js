051d5ea6b8df317af84fc754378d21525d8bc1ac
Adriana Matsufuji <adrianabm@gmail.com>
2018-07-18 14:46:02 +0200

    Add comment

--- schema/schema.js
+++ schema/schema.js
@@ -24,6 +24,8 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString }},
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`)
+          // axios response comes nested inside a 'data' property,
+          // so this is necessary to make it work nicely with GraphQL
           .then(response => response.data)
       }
     }
